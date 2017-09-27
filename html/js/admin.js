2773a8b4fe11aef8d5f9720ff1ab18a02e665df7
Rupal <webservices.intelliworkz@gmail.com>
2017-09-27 16:46:13 +0530

    admin panel chages

--- html/js/admin.js
+++ html/js/admin.js
@@ -18,6 +18,7 @@ $(function(){
 				contentType:false,
 				success:function(result){
 					alert("Added data Successfully....");
+					window.location.reload();
 				}
 		});
 
@@ -33,6 +34,7 @@ $(function(){
 				contentType:false,
 				success:function(result){
 					alert("Update data Successfully....");
+					window.location.reload();
 				}
 		});
 
@@ -72,6 +74,7 @@ $(function(){
 				contentType:false,
 				success:function(result){
 					alert("Added data Successfully....");
+					window.location.reload();
 				}
 		});
 	});
@@ -86,6 +89,7 @@ $(function(){
 				contentType:false,
 				success:function(result){
 					alert("Update data Successfully....");
+					window.location.reload();
 				}
 		});
 
@@ -121,8 +125,16 @@ $(function(){
 			"name":$("#addTypeForm #name").val()
 		};
 		$.post(baseURL+"admin/addType/",{data:data},function(data){
-			//var data=$.parseJSON(data);
-			window.location.reload();
+			var data=$.parseJSON(data);
+			if(data.status=="ok"){
+				alert("Your Main Folder added successfully...");
+				window.location.reload();
+			}
+			else if(data.status=="fail"){
+				alert("Your Main Folder added Fail...");
+				window.location.reload();
+			}
+			
 		});
 
 	});
@@ -193,9 +205,11 @@ $(function(){
 
 	$(".folder-delete-btn").on("click",function(){
 		var folderID=$(this).data('folder-id');
+		if(confirm("Do you want to delete this record..???")){
 		$.post(baseURL+"admin/deleteFolder/"+folderID,function(data){
 			$("#folder-id"+folderID).remove();
 		});
+	}
 	});
 
 	$(".folder-edit-btn").on("click",function(){
@@ -227,8 +241,8 @@ $(function(){
 			processData:false,
 			contentType:false,
 			success:function(result){
-				//alert("Added Testimonial successfully.....");
-				//window.location.reload();
+				alert("Added Testimonial successfully.....");
+				window.location.reload();
 			}
 		});
 	});
