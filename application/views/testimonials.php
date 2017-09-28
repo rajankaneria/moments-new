d64672b7f6dd4e0d94de995add630a049b5a35d1
Ravi <rmeswaniya@gmail.com>
2017-09-28 16:55:09 +0530

    Testimonials

---
+++ application/views/testimonials.php
@@ -0,0 +1,37 @@
+<div class="main-image"><div class="main-image-overlay"></div><img src="<?php echo base_url(); ?>html/images/home-bg.jpg"/></div>
+ 
+ <div class="testimonials-area">
+ 	<div class="container">
+ 		<div id="testimonial" class="carousel carousel-slider test-bg center" data-indicators="true">
+
+ 			<div class="test-arrows">
+ 				<div class="test-left"><i class="fa fa-angle-left" aria-hidden="true"></i></div>
+ 				<div class="test-right"><i class="fa fa-angle-right" aria-hidden="true"></i></div>
+ 			</div>
+		    <div class="carousel-item white-text" href="#one!">
+		      <div class="test-img">
+		      	<img src="<?php echo base_url(); ?>html/images/hirav_shah.jpg">
+		      </div>
+		      <div class="test-name">
+		      		<p>Hirav Shah</p>
+		      </div>
+		      <div class="test-desc"><p class="white-text">
+		      	We hope you have enjoyed using Materialize! If you feel Materialize has helped you out and want to support the team, send us over a donation! Any amount would help support and continue development on this project and is greatly appreciated.
+		      </p></div>
+		    </div>
+		    <div class="carousel-item white-text" href="#two!">
+		      <h2>Second Panel</h2>
+		      <p class="white-text">This is your second panel</p>
+		    </div>
+		    <div class="carousel-item white-text" href="#three!">
+		      <h2>Third Panel</h2>
+		      <p class="white-text">This is your third panel</p>
+		    </div>
+		    <div class="carousel-item white-text" href="#four!">
+		      <h2>Fourth Panel</h2>
+		      <p class="white-text">This is your fourth panel</p>
+		    </div>
+		</div>
+ 	</div>
+ </div>
+ <div class="main-footer"></div>
