1287c9d9d310d586903885cec7ff7228f3829a3b
Ravi <rmeswaniya@gmail.com>
2017-09-29 13:35:18 +0530

    Contact PAge

--- application/views/contact.php
+++ application/views/contact.php
@@ -1,36 +1,125 @@
 <div class="main-image"><div class="main-image-overlay"></div><img src="<?php echo base_url(); ?>html/images/home-bg.jpg"/></div>
 
-<div class="row">
-	<div class="form-area">
-	<div class="contact-form-area">
-		<div class="row">
-			<div class="form-title">
-				<h5>Quick Contact</h5>
+<div class="contact-area">
+	
+	<!-- form area -->
+	<div class="row">
+		<div class="col s12 m6 contact-us">
+			<div class="white-text">
+				<h5>Contact Us</h5>
 			</div>
-			<form method="post" id="contactForm">
+		<form method="post" id="contactForm">
 			<div class="input-field col s12 m6">
-	          <input id="full_name" name="full_name" type="text" class="validate" required="required">
-	          <label for="full_name">Full Name</label>
-	        </div>
-	        <div class="input-field col s12 m6">
-	          <input id="email" name="email" type="email" class="validate" required="required">
-	          <label for="email">E-mail</label>
-	        </div>
-	        <div class="input-field col s12">
-	          <input id="mobile" name="mobile" type="text" class="validate" required="required">
-	          <label for="mobile">Mobile</label>
-	        </div>
-	        <div class="input-field col s12">
-	          <textarea id="message" name="message" class="materialize-textarea"></textarea>
-          	  <label for="textarea1">Textarea</label>
-	        </div>
-	        <div class="input-field col s12">
-	          <center><input type="button" name="send" value="send" class="btn contact-btn" id="send"></center>
-	        </div>
-	        </form>
+		      <input id="full_name" name="full_name" type="text" class="validate" required="required">
+		      <label for="full_name">Full Name</label>
+		    </div>
+		    <div class="input-field col s12 m6">
+		      <input id="email" name="email" type="email" class="validate" required="required">
+		      <label for="email">E-mail</label>
+		    </div>
+		    <div class="input-field col s12">
+		      <input id="mobile" name="mobile" type="text" class="validate" required="required">
+		      <label for="mobile">Mobile</label>
+		    </div>
+		    <div class="input-field col s12">
+		      <textarea id="message" name="message" class="materialize-textarea"></textarea>
+		  	  <label for="textarea1">Message</label>
+		    </div>
+		    <div class="input-field col s12">
+		      <center><input type="button" name="send" value="send" class="btn contact-btn" id="send"></center>
+		    </div>
+		</form>
+		</div>
+
+		<!-- Frenchises Aoolication form -->
+
+		<div class="col s12 m6 franchies-area">
+			<div class="white-text">
+				<h5>Franchise Application</h5>
+			</div>
+		<form method="post" id="contactForm">
+			<div class="input-field col s12 m6">
+		      <input id="full_name" name="full_name" type="text" class="validate" required="required">
+		      <label for="full_name">Full Name</label>
+		    </div>
+		    <div class="input-field col s12 m6">
+		      <input id="email" name="email" type="email" class="validate" required="required">
+		      <label for="email">E-mail</label>
+		    </div>
+		    <div class="input-field col s12 m4">
+		      <input id="mobile" maxlength="10" name="mobile" type="text" class="validate" required="required">
+		      <label for="mobile">Mobile</label>
+		    </div>
+		    <div class="input-field col s12 m4">
+		      <input id="city" name="city" type="text" class="validate" required="required">
+		      <label for="city">City</label>
+		    </div>
+		    <div class="col s12 m4">
+				<h6 class="white-text">Business Experience</h6>
+		      <input name="group1" type="radio" id="yes" />
+      		  <label for="yes">Yes</label>&nbsp;&nbsp;
+      		  <input name="group1" type="radio" id="no" />
+      		  <label for="no">No</label>
+		    </div>
+		    <div class="input-field col s12">
+		      <textarea id="message" name="message" class="materialize-textarea"></textarea>
+		  	  <label for="textarea1">Message</label>
+		    </div>
+		    <div class="input-field col s12">
+		      <center><input type="button" name="send" value="send" class="btn contact-btn" id="send"></center>
+		    </div>
+		</form>
 		</div>
 	</div>
-</div>
+
+
+	<!-- Locationa ofiices -->
+	<div class="row">
+		<div class="col s12 m6">
+			<div class="addres-area white-text">
+				<div class="corporateOffice-area row">
+					<h5>CORPORATE OFFICE</h5>
+					<div class="location-area col s12 m6">
+						<center><i class="small material-icons">location_on</i></center>
+						<li>703, Dev Prime, B/H Divyabhaskar,</li>
+						<li>Prahladnagar, Ahmedabad</li>
+					</div>
+					<div class="phone-are col s12 m6">
+						<center><i class="small material-icons">phone</i></center>
+						<li><span>+91</span> 94290 15444</li>
+						<li><span>+91</span> 79400 33588</li>
+					</div>
+					<div class="email-area col s12 m12">
+						<center><i class="small material-icons">mail</i></center>
+						<li><a href="mailto:smile@momentsunlimited.in">smile@momentsunlimited.in</a></li>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="col s12 m6">
+			<div class="addres-area white-text">
+				<div class="branchOffice-area row">
+					<h5>BRANCH OFFICE</h5>
+					<div class="location-area col s12 m6">
+						<center><i class="small material-icons">location_on</i></center>
+						<li>703, Dev Prime, B/H Divyabhaskar,</li>
+						<li>Prahladnagar, Ahmedabad</li>
+					</div>
+					<div class="phone-are col s12 m6">
+						<center><i class="small material-icons">phone</i></center>
+						<li><span>+91</span> 94290 15444</li>
+						<li><span>+91</span> 79400 33588</li>
+					</div>
+					<div class="email-area col s12 m12">
+						<center><i class="small material-icons">mail</i></center>
+						<li><a href="mailto:smile@momentsunlimited.in">smile@momentsunlimited.in</a></li>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
 </div>
  
 <div class="main-footer"></div>
