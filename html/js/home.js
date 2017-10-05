7b5a0450619c5f1a1ade391d4b67ae81490d4d41
unknown <rajan.kaneria@gmail.com>
2017-10-05 12:47:28 +0530

    Home page js

--- html/js/home.js
+++ html/js/home.js
@@ -5,4 +5,19 @@ $(function(){
 	  }, 600, function() {
 	    // Animation complete.
 	});
+	setInterval(function(){ 
+		slideChange();
+	}, 6000);
 });
+
+function slideChange(){
+	var slideNumber = parseInt($("#sliderCount").val());
+	console.log(slideNumber);
+	$(".main-image img").removeClass("active");
+	$(".main-image img:nth-child("+slideNumber+")").addClass("active");
+	slideNumber++;
+	if(slideNumber > $(".main-image img").length){
+		slideNumber = 2;
+	}
+	$("#sliderCount").val(slideNumber);
+}
